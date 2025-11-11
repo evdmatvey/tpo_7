f0e56209e2b7c3a7a032265796f26125fdbc0dff
evdmatvey <evd.matvey@mail.ru>
2025-11-11 18:41:32 +0700

    Fix locust venv issues

--- scripts/run_locust_tests.sh
+++ scripts/run_locust_tests.sh
@@ -3,7 +3,7 @@
 cd ./tests/locust
 rm -rf locust_env
 python3 -m venv locust_env
-pip install -r requirements.txt
 source locust_env/bin/activate
+pip install -r requirements.txt
 locust -f openbmc_test.py --headless --users 10 --spawn-rate 2 --run-time 30s --host=https://localhost:2443
 cd ../../
